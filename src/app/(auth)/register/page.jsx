1b01b665ec343da3f88814f6e033bb26a7c1d44d
nyczz <niel24014@gmail.com>
2026-01-04 22:37:34 -0300

    Update page.jsx

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -31,7 +31,7 @@ export default function RegisterPage() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${window.location.origin}/inicio`,
+        redirectTo: process.env.NEXT_PUBLIC_SUPABASE_REDIRECT_URL || "/login",
       },
     });
   }
