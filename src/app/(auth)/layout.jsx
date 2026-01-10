633faa0ac64fdeab7c4d6ec4c7b4b447cd260d4b
nyczz <niel24014@gmail.com>
2026-01-10 13:22:00 -0300

    Delete src directory

--- src/app/(auth)/layout.jsx
+++
@@ -1,7 +0,0 @@
-export default function AuthLayout({ children }) {
-  return (
-    <div className="bg-black text-white min-h-screen flex items-center justify-center">
-      {children}
-    </div>
-  );
-}
--- src/configs.js
+++
@@ -1,1 +0,0 @@
-export const API_URL = "https://studdy-8yb8.onrender.com";
